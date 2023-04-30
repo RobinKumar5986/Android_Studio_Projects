c6cf8404adc27f5023aef0c61006a6612b4885dd
Robin <rkrobin6550@gmail.com>
2023-05-01 00:47:02 +0530

    Adding First Android Project To GitHub

--- app/src/main/java/com/example/phntasktwo/MainActivity.java
+++ app/src/main/java/com/example/phntasktwo/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.phntasktwo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
@@ -18,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         custumBaseAdapter custumBaseAdapter=new custumBaseAdapter(getApplicationContext(),birds);
         listView.setAdapter(custumBaseAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent=new Intent(MainActivity.this,secondView.class);
+                Toast.makeText(MainActivity.this, ""+(int)position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
